c4be1b8217c67eaceff63a89ba1e14e2ffbe8418
Ali Bouzar <alibouzar@foi.hr>
2021-02-17 08:20:31 +0100

    use heaser and sidebar as shared files using thymeleaf segment

--- src/main/java/com/miaoubich/boot/MyController.java
+++ src/main/java/com/miaoubich/boot/MyController.java
@@ -10,5 +10,54 @@ public class MyController {
 	public String home() {
 		return "index";
 	}
+	
+	@GetMapping("/basictable")
+	public String getBasicTable() {
+		return "basic_table";
+	}
+	
+	@GetMapping("/buttons")
+	public String getButtons() {
+		return "buttons";
+	}
+	
+	@GetMapping("/component")
+	public String getComponents() {
+		return "form_component";
+	}
+	
+	@GetMapping("/charts")
+	public String getCharts() {
+		return "chart-charts";
+	}
 
+	@GetMapping("/form-validation")
+	public String getformValidation() {
+		return "form_validation";
+	}
+	
+	@GetMapping("/general")
+	public String getGeneral() {
+		return "general";
+	}
+	
+	@GetMapping("/grids")
+	public String getGrids() {
+		return "grids";
+	}
+	
+	@GetMapping("/login")
+	public String getlogin() {
+		return "login";
+	}
+	
+	@GetMapping("/profile")
+	public String getprofile() {
+		return "profile";
+	}
+	
+	@GetMapping("/widgets")
+	public String getwidgets() {
+		return "widgets";
+	}
 }
