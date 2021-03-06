bd8221f0be7da4a20799abcd0b855fcf622d917f
Ali Bouzar <alibouzar@foi.hr>
2021-03-06 18:10:27 +0100

    add state html page, controller and servie

--- src/main/java/com/miaoubich/boot/controllers/StateController.java
+++ src/main/java/com/miaoubich/boot/controllers/StateController.java
@@ -1,13 +1,56 @@
 package com.miaoubich.boot.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.miaoubich.boot.models.State;
+import com.miaoubich.boot.services.StateService;
 
 @Controller
 public class StateController {
 
-	@GetMapping("/state")
-	public String home() {
+	@Autowired
+	private StateService stateService;
+
+	@GetMapping("/states")
+	public String displayStates(Model model) {
+
+	List<State> statesList = stateService.getStates();
+	model.addAttribute("states", statesList);
 		return "state";
 	}
+	
+	@PostMapping("/state/addNew")
+	public String addNewState(State state) {
+		stateService.saveState(state);
+		return "redirect:/states";
+	}
+	
+	@GetMapping("/state/findById")
+	@ResponseBody
+	public Optional<State> findStateById(int id){
+		return stateService.findById(id);
+	}
+	
+	@RequestMapping(value="/state/update", method= {RequestMethod.PUT, RequestMethod.GET})
+	public String updateState(State state) {
+		stateService.saveState(state);
+		return "redirect:/states";
+	}
+	
+	@RequestMapping(value="/state/delete", method= {RequestMethod.DELETE, RequestMethod.GET})
+	public String deleteState(int id) {
+		stateService.deleteState(id);
+		return "redirect:/states";
+	}
+
 }
--- src/main/java/com/miaoubich/boot/services/StateService.java
+++ src/main/java/com/miaoubich/boot/services/StateService.java
@@ -1,8 +1,34 @@
 package com.miaoubich.boot.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.miaoubich.boot.models.State;
+import com.miaoubich.boot.repositories.StateRepository;
 
 @Service
 public class StateService {
+
+	@Autowired 
+	StateRepository stateRepository;
+	
+	public List<State> getStates() {
+		return stateRepository.findAll();
+	}
+
+	public void saveState(State state) {
+		stateRepository.save(state);		
+	}
+
+	public Optional<State> findById(int id) {
+		return stateRepository.findById(id);
+	}
+
+	public void deleteState(int id) {
+		stateRepository.deleteById(id);
+	}
 
 }
